baac6654166df876ae8ac5573490779737a777e7
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-11-07 12:01:28 +0600

    Post data done

--- src/Pages/AddCoffee.jsx
+++ src/Pages/AddCoffee.jsx
@@ -12,15 +12,29 @@ const AddCoffee = () => {
         const chef = form.chef.value;
         const taste = form.taste.value;
         const details = form.details.value;
-        const coffee = {name, supplier, category, chef, taste, details,};
-        console.log(coffee);
+        const newCoffee = {name, supplier, category, chef, taste, details,};
+        console.log(newCoffee);
+
+
+        //post coffee in server
+        fetch('http://localhost:5000/coffee',{
+            method : 'POST',
+            headers : {
+                "content-type" : "application/json"
+            },
+            body : JSON.stringify(newCoffee)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
     }
 
     return (
-        <div className="max-w-[90%] xl:max-w-[1000px] mx-auto mb-16">
+        <div className="max-w-[90%] xl:max-w-[1000px] mx-auto mb-16 font-Railway">
             <Link to='/' className="font-Rancho flex gap-2 items-center mt-8 mb-8 text-xl"><IoArrowBack></IoArrowBack> Back to Home</Link>
 
-            <div className="w-full bg-[#F4F3F0] font-Railway p-10">
+            <div className="w-full bg-[#F4F3F0] font-Railway p-10 rounded-xl">
                 
                 <h1 className="text-center font-Rancho text-3xl font-bold">Add New Coffee</h1>
                 <p className="text-center sm:max-w-3xl mx-auto mt-3 text-gray-600">It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.</p>
@@ -67,6 +81,10 @@ const AddCoffee = () => {
                         </div>
                     </div>
 
+                    <div className="mb-2">
+                        <h3 className="font-semibold mb-3 mt-4">photoURL : </h3>
+                        <input className="w-full p-3 rounded-lg" type="text" name="photoURL" id="photoURL" placeholder="Enter coffee Details" />
+                    </div>
                     
                     <button type="submit" className="font-Rancho font-semibold text-xl w-full bg-[#D2B48c] text-center py-2 mt-4 rounded-lg hover:border-2 hover:border-[#382828]">Add Coffee</button>
 
