a23819029e4bbb8353a7c174c9173ca947169649
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-11-07 18:31:57 +0600

    Delete function Done

--- src/components/Coffee.jsx
+++ src/components/Coffee.jsx
@@ -2,13 +2,44 @@
 import { IoEyeSharp } from "react-icons/io5";
 import { FaPen } from "react-icons/fa6";
 import { MdDeleteForever } from "react-icons/md";
+import Swal from "sweetalert2";
 
 
 const Coffee = ({coffee}) => {
 
-    const {name, chef, photoURL, price} = coffee;
+    const {_id, name, chef, photoURL, price} = coffee;
 
-    console.log(coffee)
+    const handleDelete = id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                
+
+
+                fetch(`http://localhost:5000/coffee/${_id}`,{
+                    method : 'DELETE'
+                })
+                .then(res => res.json())
+                .then(data =>{
+                    console.log(data);
+                    if(data.deletedCount > 0){
+                        Swal.fire({
+                            title: "Deleted!",
+                            text: "Your coffee has been deleted.",
+                            icon: "success"
+                        });
+                    }
+                })           
+            }
+        });
+    }
 
     return (
         <div className="bg-[#f4f3f097] p-4 rounded-lg font-Railway lg:flex justify-between items-center cursor-pointer hover:border-2 hover:border-[#382828] transition hover:scale-105 shadow-lg">
@@ -32,7 +63,7 @@ const Coffee = ({coffee}) => {
                     <FaPen></FaPen>
                 </button>
                 
-                <button className="bg-[red] h-8 w-8 flex items-center justify-center text-xl text-white">
+                <button onClick={() => {handleDelete(_id)}} className="bg-[red] h-8 w-8 flex items-center justify-center text-xl text-white">
                     <MdDeleteForever></MdDeleteForever>
                 </button>
             </div>
